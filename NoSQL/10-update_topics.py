1223af5ffa3321dac3a2bed07b9d5040f61e30d7
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2026-04-17 16:26:24 -0600

    Finished NoSQL

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module for using PyMongo """
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes field in document """
+    name_field = {"name": name}
+    value_field = {"$set": {"topics": topics}}
+    mongo_collection.update_many(name_field, value_field)
---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Module for using PyMongo """
+
+def schools_by_topic(mongo_collection, topic):
+    """ Returns the list of school """
+    return [item for item in mongo_collection.find({"topics": topic})]
